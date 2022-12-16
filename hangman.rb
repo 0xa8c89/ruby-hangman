d77701288add47dead61bcf92ba9c3bad089913f
0xa8c89 <117485263+0xa8c89@users.noreply.github.com>
2022-12-16 12:39:30 +0100

    Make infinite game loop

--- hangman.rb
+++ hangman.rb
@@ -7,3 +7,15 @@ end
 words = File.open('google-10000-english-no-swears.txt', 'r').readlines
 
 word = get_random_word(words)
+unknown = ['_'] * word.length
+
+until word.split('') == unknown
+  print 'Enter your guess: '
+  input = gets.chomp
+
+  word.split('').each_with_index do |letter, idx|
+    unknown[idx] = input if letter == input
+  end
+
+  puts unknown.join(' ')
+end
